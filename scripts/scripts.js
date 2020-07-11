b0ca58aa85fdc11afc029298a67ffb1dbc10b2f8
Katya Zhukova <ekaterinazhukova95@yandex.ru>
2020-07-12 01:33:13 +0300

    первый коммит

---
+++ scripts/scripts.js
@@ -0,0 +1,33 @@
+let popupForm = document.querySelector('.popup__form');
+let userName = popupForm.querySelector('.popup__name-form');
+let userJob = popupForm.querySelector('.popup__job-form');
+let popup = document.querySelector('.popup');
+let closeButton = popup.querySelector('.popup__close-button');
+let profile = document.querySelector('.profile');
+let editButton = profile.querySelector('.profile__edit-button');
+let currentUserName = profile.querySelector('.profile__name');
+let currentUserJob = profile.querySelector('.profile__job');
+let saveButton = popup.querySelector('.popup__save-button');
+
+function openPopup() {
+    popup.classList.add('popup_opened');
+    userName.value = currentUserName.textContent;
+    userJob.value = currentUserJob.textContent;
+}
+
+editButton.addEventListener('click', openPopup);
+
+function closePopup() {
+    popup.classList.remove('popup_opened');
+}
+
+closeButton.addEventListener('click', closePopup);
+
+function formSubmitHandler(evt) {
+    evt.preventDefault();
+    currentUserName.textContent = userName.value;
+    currentUserJob.textContent = userJob.value;
+    closePopup();
+}
+
+saveButton.addEventListener('click', formSubmitHandler);
